203d1352024525449c59b893c48a769b3b50f9a0
tvolex <tisyanchin13@gmail.com>
2018-11-08 23:48:10 +0200

    feat(navbar)backgoud for navbar

--- src/components/Dashboard/Navbar.vue
+++ src/components/Dashboard/Navbar.vue
@@ -4,7 +4,7 @@
 
             <v-toolbar-side-icon @click="statusNavBar = !statusNavBar"></v-toolbar-side-icon>
 
-            <v-toolbar-title class="white--text">Dashboard</v-toolbar-title>
+            <v-toolbar-title >Dashboard</v-toolbar-title>
 
             <v-spacer></v-spacer>
 
@@ -22,60 +22,57 @@
                 <v-icon>more_vert</v-icon>
             </v-btn>
         </v-toolbar>
-        <v-navigation-drawer absolute temporary v-model="statusNavBar">
-            <v-list-tile avatar>
-                <v-list-tile-avatar>
-                    <img :src="user.avatar">
-                </v-list-tile-avatar>
+            <v-navigation-drawer dark class="imgForNavbar" absolute temporary v-model="statusNavBar">
+                <v-list dark dense>
+                    <v-list-tile avatar>
+                        <v-list-tile-avatar>
+                            <img :src="user.avatar">
+                        </v-list-tile-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title> {{user.name}} </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider></v-divider>
+                    <v-list-tile
+                            v-for="item in dashboardNavigation"
+                            :key="item.title"
+                            v-bind:class="item.route === route ? selectedTab: ''"
+                            class="pa-2"
+                    >
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon >
+                        </v-list-tile-action>
 
-                <v-list-tile-content>
-                    <v-list-tile-title> {{user.name}} </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+                        <v-list-tile-content >
+                            <router-link tag="v-list-tile-title" :to="item.route">
+                                {{ item.title }}
+                            </router-link>
+                        </v-list-tile-content>
 
-            <v-divider></v-divider>
+                    </v-list-tile>
+                    <v-divider></v-divider>
+                    <v-list-tile
+                            v-for="item in userNavigation"
+                            :key="item.title"
+                            v-bind:class="item.route === route ? selectedTab: ''"
+                            class="pa-2"
+                    >
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
 
-            <v-list dense class="pt-0">
-                <v-list-tile
-                        v-for="item in dashboardNavigation"
-                        :key="item.title"
-                        @click=""
-                >
-                    <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
-
-                    <v-list-tile-content>
-                        <router-link tag="v-list-tile-title" :to="item.route">
-                            {{ item.title }}
-                        </router-link>
-                    </v-list-tile-content>
-
-                </v-list-tile>
-            </v-list>
-            <v-divider></v-divider>
-            <v-list dense class="pt-0">
-                <v-list-tile
-                        v-for="item in userNavigation"
-                        :key="item.title"
-                        @click=""
-                >
-                    <v-list-tile-action>
-                        <v-icon>{{ item.icon }}</v-icon>
-                    </v-list-tile-action>
-
-                    <v-list-tile-content>
-                        <v-list-tile-title v-if="item.title === 'Вихід'" @click="logout">
-                            {{ item.title }}
-                        </v-list-tile-title>
-                        <router-link v-else tag="v-list-tile-title" :to="item.route">
-                            {{ item.title }}
-                        </router-link>
-                    </v-list-tile-content>
-
-                </v-list-tile>
-            </v-list>
-        </v-navigation-drawer>
+                        <v-list-tile-content>
+                            <v-list-tile-title v-if="item.title === 'Вихід'" @click="logout">
+                                {{ item.title }}
+                            </v-list-tile-title>
+                            <router-link v-else tag="v-list-tile-title" :to="item.route">
+                                {{ item.title }}
+                            </router-link>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                    <v-divider></v-divider>
+                </v-list>
+            </v-navigation-drawer>
     </div>
 
 
@@ -95,15 +92,21 @@
 					{title: 'Календар', icon: 'date_range', route: '/dashboard/calendar'},
 					{title: 'Менеджмент', icon: 'people', route: '/dashboard/management'},
 					{title: 'Новий пацієнт', icon: 'add', route: '/dashboard/newPatient'},
-					{title: 'Знайти пацієнта', icon: 'insert_drive_file', route: '/dashboard/findPatient'},
+					{title: 'Знайти пацієнта', icon: 'insert_drive_file', method: 'logout', route: '/dashboard/findPatient'},
 				],
                 userNavigation: [
                     {title: 'Налаштування', icon: 'settings', route: '/dashboard/settings'},
                     {title: 'Вихід', icon: 'exit_to_app', route: '/'},
                 ],
+                isSelected: false,
+                selectedTab: 'selectedTab'
             }
         },
-
+        computed: {
+			route: function () {
+				return this.$route.fullPath;
+			}
+        },
         methods: {
             logout: function () {
                  this.$store.dispatch({type: "logout"});
@@ -113,3 +116,17 @@
         },
     }
 </script>
+
+<style scoped>
+    .imgForNavbar {
+        background-image: url("../../assets/img/sidebar-2.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    .selectedTab {
+        background: rgba(0,255,0,0.4);
+    }
+    .v-navigation-drawer .v-list {
+        background:transparent;
+    }
+</style>

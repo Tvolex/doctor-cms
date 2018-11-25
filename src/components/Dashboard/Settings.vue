834559e804c81dd63520d5e08aa47c3fecfb64df
tvolex <tisyanchin13@gmail.com>
2018-11-25 17:06:40 +0200

    fix(settings): field validation

--- src/components/Dashboard/Settings.vue
+++ src/components/Dashboard/Settings.vue
@@ -5,7 +5,7 @@
                 <h2 class="pa-2">
                     Налаштування
                 </h2>
-                <v-form v-model="isFormValid" name="settingsForm">
+                <v-form v-model="isFormValid" ref="settingsForm" name="settingsForm" :lazy-validation="false">
                     <v-layout justify-center align-center wrap>
                         <v-flex xs12 sm7 >
                             <v-text-field
@@ -52,19 +52,36 @@
                         <v-flex xs1>
                             <v-icon @click="disablePatronymic = !disablePatronymic">edit</v-icon>
                         </v-flex>
-                        <v-flex xs11 sm7>
+                        <v-flex xs11 sm7 >
                             <v-text-field
                                     v-model="password"
                                     value="random password"
+                                    ref="password"
                                     :disabled="disablePassword"
+                                    :rules="passwordRules"
                                     label="Пароль"
-                                    type="password"
                                     name="password"
+                                    :type="typeOfFieldForPassword ? 'text' : 'password'"
+                                    :append-icon="typeOfFieldForPassword ? 'visibility_off' : 'visibility'"
+                                    @click:append="typeOfFieldForPassword = !typeOfFieldForPassword"
                             ></v-text-field>
                         </v-flex>
                         <v-flex xs1>
                             <v-icon @click="disablePassword = !disablePassword">edit</v-icon>
                         </v-flex>
+                        <v-scroll-y-reverse-transition mode="out-in">
+                            <v-flex xs11 sm7 v-if="!disablePassword" >
+                                <v-text-field
+                                        v-model="confirmPassword"
+                                        :rules="confirmPasswordRules"
+                                        label="Повторіть пароль"
+                                        name="confirmPassword"
+                                        :type="typeOfFieldForConfirmPassword ? 'text' : 'password'"
+                                        :append-icon="typeOfFieldForConfirmPassword ? 'visibility_off' : 'visibility'"
+                                        @click:append="typeOfFieldForConfirmPassword = !typeOfFieldForConfirmPassword"
+                                ></v-text-field>
+                            </v-flex>
+                        </v-scroll-y-reverse-transition>
                         <v-flex xs12 align-center>
                             <v-btn flat>
                                 <input type="file" @change="onFileChanged">
@@ -75,7 +92,7 @@
                         <v-flex xs12>
                             <v-btn large round color="success"
                                    :loading="loading"
-                                   :disabled="!wasChanges || loading"
+                                   :disabled="!wasChanges && !isFormValid || loading"
                                    @click="Save">
                                 Зберегти
                                 <span slot="loader" class="custom-loader">
@@ -111,11 +128,15 @@
 				disablePatronymic: true,
 				disablePassword: true,
 
+				typeOfFieldForPassword: false,
+				typeOfFieldForConfirmPassword: false,
+
 				email: this.user.email,
 				name: this.user.name,
 				surname: this.user.surname,
 				patronymic: this.user.patronymic,
-                password: '',
+                password: null,
+				confirmPassword: null,
 
 				selectedFile: null,
 				avatar: null,
@@ -124,6 +145,14 @@
 					v => !!v || 'E-mail обов`язковий!',
 					v => /.+@.+/.test(v) || 'E-mail повинен бути валідним!'
 				],
+				passwordRules: [
+					v => !!v && !this.disablePassword || 'Пароль обов`язковий!',
+				],
+                confirmPasswordRules: [
+					v => !!v || 'Повторний пароль обов`язковий!',
+					v => _.isEqual(v, this.password) || 'Пароль не співпадає!',
+				],
+
 				nameRules: [v => !!v || 'Імя обов`язкове!'],
 				surnameRules: [v => !!v || 'Прізвище обов`язкове!'],
 				patronymicRules: [v => !!v || 'По батькові обов`язкове!'],
@@ -165,8 +194,11 @@
 						avatar: this.avatar,
                     }).then(res => {
 						console.log(res.data);
+						this.$notificator(res.data.type, res.data.message);
+                        this.$store.commit('user', {type: 'user', value: res.data.value});
 					}).catch(err => {
 						console.log(err);
+						this.$notificator('error', err.message)
 					})
 				}
             }
@@ -176,9 +208,6 @@
         },
         watch: {
 			name(name, oldValue) {
-				console.log(name);
-				console.log(oldValue);
-				console.log(this.user.name);
 				if (!_.isEqual(name, this.user.name)) {
 					this.wasChanges = true;
                 }
@@ -193,12 +222,43 @@
 					this.wasChanges = true;
 				}
 			},
-			password(password, oldValue) {
-				this.wasChanges = true;
-			},
 			avatar(avatar, oldValue) {
-                this.wasChanges = true;
+				if (avatar) {
+					this.wasChanges = true;
+				}
 			},
+			password(password, oldValue) {
+				if (_.isEqual(password, this.confirmPassword)) {
+					this.wasChanges = true;
+				}
+			},
+			confirmPassword(confirmPassword, oldValue) {
+				if (_.isEqual(confirmPassword, this.password)) {
+					this.wasChanges = true;
+				}
+			},
+			disableName(value, oldValue) {
+				if (value) {
+					this.name = this.user.name;
+				}
+			},
+			disableSurname(value, oldValue) {
+				if (value) {
+					this.surname = this.user.surname;
+				}
+			},
+			disablePatronymic(value, oldValue) {
+				if (value) {
+					this.patronymic = this.user.patronymic;
+				}
+			},
+			disablePassword(value, oldValue) {
+				if (value) {
+					this.password = null;
+					this.confirmPassword = null;
+					this.$refs.password.reset();
+                }
+            }
         }
 	}
 </script>

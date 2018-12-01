7a35a133c78a2a581f027189ec7db9c97d4ad286
tvolex <tisyanchin13@gmail.com>
2018-12-01 14:46:26 +0200

    update calendar

--- src/components/Dashboard/Calendar.vue
+++ src/components/Dashboard/Calendar.vue
@@ -1,19 +1,82 @@
 <template>
     <v-container fluid>
         <v-layout row wrap main-items>
-            <v-flex xs12 md8 offset-md2 lg6 offset-lg3>
+            <v-flex class="calendar pa-3" xs12 md8 offset-md2 lg6 offset-lg3>
                 <full-calendar :events="events"
                                listWeek
-                               v-model="calendar"
+                               :config="config"
+                               @event-selected="eventSelected"
+                               @view-render="getEvents"
                                ref="calendar"></full-calendar>
             </v-flex>
         </v-layout>
+        <v-dialog
+                v-if="selectedEvent"
+                v-model="eventDetailsDialog"
+                max-width="600px"
+        >
+            <v-card>
+                <v-card-title class="headline">Деталі запису</v-card-title>
+                <v-layout row wrap justify-center>
+                    <v-flex xs10>
+                        <v-avatar
+                                size="100"
+                                class="ma-2"
+                        >
+                            <img v-if="selectedEvent.patient.avatar" :src="selectedEvent.patient.avatar">
+                            <img v-else src="@/assets/person.png">
+                        </v-avatar>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>Пацієнт:</b> {{selectedEvent.patient.fullName}}
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>Email пацієнта:</b> {{selectedEvent.patient.email}}
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>Телефон пацієнта:</b> {{selectedEvent.patient.contact}}
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>До:</b> {{selectedEvent.specialization}}
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>Статус:</b> {{selectedEvent.status}}
+                        </v-card-text>
+                    </v-flex>
+                    <v-flex xs10>
+                        <v-card-text class="pa-1">
+                            <b>Дата:</b> {{selectedEvent.fullDate.replace(':', " година: ")}}
+                        </v-card-text>
+                    </v-flex>
+                </v-layout>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                            flat="flat"
+                            color="green"
+                            @click="eventDetailsDialog = false"
+                    >
+                        Закрити
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-container>
 </template>
 
 <script>
     import axios from 'axios';
     import moment from 'moment';
+	import 'fullcalendar/dist/locale/uk'
     export default {
         name: "calendar",
         props: {
@@ -42,35 +105,52 @@
         },
         data() {
             return {
-				calendar: null,
-                events: [
-                    {
-                        title  : 'event1',
-                        start  : '2018-10-31T12:30:00',
-                        end  : '2018-10-31T13:30:00',
-                        color: 'pink',
-                        textColor: 'black',
-                    },
-                    {
-                        title  : 'event2',
-                        start  : '2018-11-1T14:00:00',
-                        end  : '2018-11-1T15:00:00',
-                    },
-                    {
-                        title  : 'event3',
-                        start  : '2018-10-31T11:00:00',
-                        allDay : true,
-                    },
-                ]
+				eventDetailsDialog: false,
+				selectedEvent: null,
+				config: {
+					weekends: false,
+					editable: false,
+					selectable: false,
+					selectHelper: false,
+					locale: 'uk',
+				},
+                events: []
             }
         },
 
         methods: {
-        	getEvents() {
+			eventSelected(event, jsEvent, view) {
+				this.selectedEvent = event;
+				this.eventDetailsDialog = true;
+            },
+        	getEvents(calendarView, element) {
+        		console.log(calendarView);
+				console.log(element);
+        		let fromDate = null;
+				let toDate = null;
+        		switch (calendarView.name) {
+					case "agendaDay":
+						fromDate = moment(new Date(calendarView.intervalStart._i)).startOf('day').format("YYYY-MM-DD:HH-mm");
+						toDate =  moment(new Date(calendarView.intervalEnd._i)).endOf('day').format("YYYY-MM-DD:HH-mm");
+						break;
+                    case "agendaWeek":
+                        fromDate = moment(new Date(calendarView.intervalStart._i)).startOf('week').format("YYYY-MM-DD:HH-mm");
+						toDate = moment(new Date(calendarView.intervalEnd._i)).endOf('week').format("YYYY-MM-DD:HH-mm");
+						break;
+					case "month":
+						fromDate = moment(new Date(calendarView.intervalStart._i)).startOf('month').format("YYYY-MM-DD:HH-mm");
+						toDate = moment(new Date(calendarView.intervalEnd._i)).endOf('month').format("YYYY-MM-DD:HH-mm");
+						break;
+					default:
+						fromDate = moment().startOf('month').format("YYYY-MM-DD:HH-mm");
+						toDate = moment().endOf('month').format("YYYY-MM-DD:HH-mm");
+						break;
+				}
 				axios.get('/api/event', {
 					params: {
-						fromDate: null,
-						toDate: null,
+						fromDate,
+						toDate,
+						doctor: this.user._id,
 					}
 				}).then((res) => {
 					this.events = res.data.map(event => {
@@ -80,6 +160,7 @@
 						event.end = moment(event.fullDate, "YYYY-MM-DD:HH-mm")
                             .set("hour", moment(event.fullDate, "YYYY-MM-DD:HH-mm").get("hour") + 1)
                             .format();
+						return event;
                     });
 				}).catch(err => {
 					console.log(err);
@@ -92,6 +173,9 @@
 </script>
 
 <style >
+    .calendar {
+        background-color: oldlace;
+    }
     .fc-time-grid-container, .fc-time-grid-container {
         max-height: 600px !important;
         min-height: 500px !important;

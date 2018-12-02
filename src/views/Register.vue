c2c2f9dde3a77fafff4d5d6187d6192c4062ede9
Tvolex <tisyanchin13@gmail.com>
2018-12-02 23:13:57 +0200

    Update Register.vue
    Make field of apartment as text, not number

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -140,7 +140,6 @@
                             <v-text-field
                                     v-model="apartment"
                                     label="Номер квартири"
-                                    type="number"
                                     name="apartment"
                                     outline
                                     required
